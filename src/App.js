a081c25427e1cecfeeda228c95e6c609ba123915
Volodymyr Kurov <151948182+vldkurov@users.noreply.github.com>
2024-03-20 12:32:52 +0000

    Add image upload functionality and improve styling
    The app's main page was restructured to support image upload and display. This feature relies on React hooks, form data, and axios for async API calls. Also, the styling in App.css was adjusted to match the new layout of App's main page. A new NPM package, axios, was installed and a new rule was added to .gitignore.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [imageUrl, setImageUrl] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    const handleFileSelect = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        const formData = new FormData();
+        formData.append('image', selectedFile);
+
+        try {
+            const response = await axios.post('http://localhost:5001/detect', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+                responseType: 'blob', // Handle the binary response
+            });
+            const imageUrl = URL.createObjectURL(response.data);
+            setImageUrl(imageUrl);
+        } catch (error) {
+            console.error('Error uploading image:', error);
+        } finally {
+            setIsLoading(false); // Завершение загрузки
+        }
+    };
+
+
+    return (
+        <div className="App">
+            <h1>Upload Image</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="file" onChange={handleFileSelect}/>
+                <button type="submit">Upload</button>
+            </form>
+            <div className="image-container">
+                {isLoading && <div className="loader"></div>}
+                {imageUrl && <img src={imageUrl} alt="Processed" style={{maxWidth: '100%', maxHeight: '400px'}}/>}
+            </div>
+        </div>
+    );
 }
 
 export default App;
